587a65e0dd62955c7433266baa9f9bfd124379c5
TCarey03 <tylercarey03@gmail.com>
2026-09-06 22:54:46 -0500

    Add calculateStoreTax function for tax calculation
    Phase 2 completed.

--- script.js
+++ script.js
@@ -2,14 +2,20 @@ function calculatePrice(price, quantity) {
     return price * quantity;
 }
 
-// Function expression
 const getTax = function(amount, taxRate) {
     return amount * taxRate;
 };
 
-// Console tests
 const subtotal = calculatePrice(15, 4);
 const tax = getTax(subtotal, 0.07);
 
 console.log("Subtotal:", subtotal);
 console.log("Tax:", tax);
+
+function calculateStoreTax(amount) {
+    const defaultTaxRate = 0.07;
+
+    return getTax(amount, defaultTaxRate);
+}
+
+console.log("Store Tax:", calculateStoreTax(100));
